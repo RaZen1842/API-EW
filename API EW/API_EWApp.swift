dfa05103ff52a369dddc72208e430029d83fd78f
Shreyas Veturi <veturi.s@etoncollege.org.uk>
2024-06-04 13:07:27 +0100

    Initial Commit

---
+++ API EW/API_EWApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_EWApp.swift
+//  API EW
+//
+//  Created by Shreyas Veturi on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct API_EWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API EW/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  API EW
+//
+//  Created by Shreyas Veturi on 04/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
